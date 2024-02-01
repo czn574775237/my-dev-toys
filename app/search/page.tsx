79409940983aba42b71a5d65f9f315f9c214fba5
zhining on home <574775237@qq.com>
2024-02-01 22:13:58 +0800

    chmod(deps) fixed chatgpt prompt

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -30,7 +30,8 @@ type FeaturePromptItem = { id: string; name: string; prompt: string };
 const featurePrompts: FeaturePromptItem[] = [
   {
     id: "translate",
-    prompt: "翻译成中文",
+    prompt: `我想让你充当中文翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，翻译它并用我的文本的更正和改进版本用中文回答。我希望你用更优美优雅的高级英语单词和句子替换我简化的 A0 级单词和句子。保持相同的意思，但使它们更文艺。我要你只回复更正、改进，不要写任何解释。
+      我的第一句话是“istanbulu cok seviyom burada olmak cok guzel`,
     name: "翻译",
   },
   {
@@ -100,6 +101,7 @@ export default function Page() {
 
   const onAdjustWindow = async () => {
     const { appWindow, LogicalSize } = await getAppWindow();
+    // appWindow.
     await appWindow.setSize(new LogicalSize(500, 500));
   };
 
